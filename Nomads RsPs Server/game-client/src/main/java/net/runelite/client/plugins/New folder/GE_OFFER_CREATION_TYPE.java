b17b6d352c3be5da1959dbe2082d2a199e44e73c
SJMercilus <me9.trickshots5@gmail.com>
2026-04-26 22:28:25 -0500

    Delete Nomads RsPs Server directory

--- Nomads RsPs Server/game-client/src/main/java/net/runelite/client/plugins/New folder/GE_OFFER_CREATION_TYPE.java
+++
@@ -1,4 +0,0 @@
-package net.runelite.client.plugins.itemstats;
-
-public class GE_OFFER_CREATION_TYPE {
-}
--- Nomads RsPs Server/game-server/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title Tarnish Server
-java -XX:-OmitStackTraceInFastThrow --enable-preview -XX:+UseZGC -Xmx8g -Xms4g -jar build\libs\game-server-all.jar
-pause
